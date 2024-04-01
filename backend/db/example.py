f9e68eaf364b3b0900f0d3c3dec73240dd71faf1
Ayden Boyko <ajb7196@rit.edu>
2024-04-01 13:54:53 -0400

    fullstack started

--- backend/db/example.py
+++ backend/db/example.py
@@ -4,9 +4,3 @@ from .db_utils import *
 def rebuild_tables():
     exec_sql_file('backend/db/schema.sql')
     exec_sql_file('backend/db/test_data.sql')
-
-def list_examples():
-    """This is an example. Please remove from your code before REST1 deadline.
-    DB layer call for listing all rows of our example.
-    """
-    return exec_get_all('SELECT id, foo FROM example_table')
